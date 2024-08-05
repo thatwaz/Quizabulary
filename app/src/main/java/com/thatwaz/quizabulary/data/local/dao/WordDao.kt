25194dbf34debbb903fe56846be2f7f2ff8a2620
thatWaz <wazowski0425@gmail.com>
2024-08-05 00:06:16 -0400

    Updated WordDao and added .gitignore files

--- app/src/main/java/com/thatwaz/quizabulary/data/local/dao/WordDao.kt
+++ app/src/main/java/com/thatwaz/quizabulary/data/local/dao/WordDao.kt
@@ -1,11 +1,12 @@
 package com.thatwaz.quizabulary.data.local.dao
 
 
-import androidx.lifecycle.LiveData
+
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
 import androidx.room.Transaction
+import kotlinx.coroutines.flow.Flow
 import com.thatwaz.quizabulary.data.local.Word
 import com.thatwaz.quizabulary.data.local.WordWithDefinitions
 
@@ -16,12 +17,11 @@ interface WordDao {
     suspend fun insertWord(word: Word)
 
     @Query("SELECT * FROM words WHERE isComplete = 0")
-    fun getIncompleteWords(): LiveData<List<Word>>
+    fun getIncompleteWords(): Flow<List<Word>>
 
     @Transaction
     @Query("SELECT * FROM words WHERE id = :wordId")
-    fun getWordWithDefinitions(wordId: Int): LiveData<WordWithDefinitions>
-
-    // Other CRUD operations...
+    fun getWordWithDefinitions(wordId: Int): Flow<WordWithDefinitions>
 }
+
 
